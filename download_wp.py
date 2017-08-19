3d47e694365365d751a531f820da2bfcba4e42f5
zhongjiajie <zhongjiajie955@hotmail.com>
2017-08-19 22:56:13 +0800

    新增download_wp.py调用主模块

---
+++ download_wp.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""Usage:
+  download_wp.py [-n=<num>] [-s=<size>] [-t=<timeout>] [-m=<max_retry_time>] [-p=<path>]
+  download_wp.py (--help | -h)
+
+Options:
+  --help -h                              Show this screen.
+  --num -n=<num>                         Number of picture to download, default 10.
+  --size -s=(size)                       Size of download picture, `small` or `big`, default `small`.
+  --timeout -t=<timeout>                 Timeout of request, default 60.
+  --max_retry_time -m=<max_retry_time>   Max Retry Time of request, default 3.
+  --path -p=<path>                       Path to download picture, default `$project/pic/`.
+"""
+
+from docopt import docopt
+
+from wallpaper import WallPaper
+
+
+def main():
+    args = docopt(__doc__, version='wallpaper ')
+
+    kwargs = {
+        'url': 'http://www.socwall.com/',
+        'num': args['--num'],
+        'size': args['--size'],
+        'path': args['--path'],
+        'timeout': args['--timeout'],
+        'max_retry_time': args['--max_retry_time']
+    }
+
+    wp = WallPaper(**kwargs)
+    wp.parse()
+
+if __name__ == '__main__':
+    main()
